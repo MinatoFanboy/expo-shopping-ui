0a818a1274d28591c4059dbfaac920451ac84f44
MinatoFanboy <thanh31299@gmail.com>
2024-11-12 16:57:55 +0700

    Assets

---
+++ components/Text.tsx
@@ -0,0 +1,112 @@
+import { FC } from 'react';
+
+/** React Native */
+import { StyleSheet, Text as RnText, type TextProps } from 'react-native';
+
+/** Hook Theme */
+import { useThemeColor } from '@/hooks/useThemeColor';
+
+export type ThemedTextProps = TextProps & {
+    bold?: boolean;
+    darkColor?: string;
+    lightColor?: string;
+    type?:
+        | 'default'
+        | 'header1'
+        | 'header2'
+        | 'header3'
+        | 'header4'
+        | 'header5'
+        | 'header6'
+        | 'header7'
+        | 'body1'
+        | 'body2'
+        | 'caption'
+        | 'label';
+};
+
+const Text: FC<ThemedTextProps> = ({ bold, darkColor, lightColor, style, type = 'default', ...rest }) => {
+    const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+
+    return (
+        <RnText
+            style={[
+                { color },
+                bold ? styles.bold : undefined,
+                /** Font Style */
+                type === 'default' ? styles.default : undefined,
+                /** Header */
+                type === 'header1' ? styles.header1 : undefined,
+                type === 'header2' ? styles.header2 : undefined,
+                type === 'header3' ? styles.header3 : undefined,
+                type === 'header4' ? styles.header4 : undefined,
+                type === 'header5' ? styles.header5 : undefined,
+                type === 'header6' ? styles.header6 : undefined,
+                type === 'header7' ? styles.header7 : undefined,
+                /** Body */
+                type === 'body1' ? styles.body1 : undefined,
+                type === 'body2' ? styles.body2 : undefined,
+                /** Caption */
+                type === 'caption' ? styles.caption : undefined,
+                /** Label */
+                type === 'label' ? styles.label : undefined,
+                style,
+            ]}
+            {...rest}
+        />
+    );
+};
+
+const styles = StyleSheet.create({
+    body1: {
+        fontSize: 18,
+        lineHeight: 28,
+    },
+    body2: {
+        fontSize: 14,
+        lineHeight: 20,
+    },
+    bold: {
+        fontWeight: 'bold',
+    },
+    caption: {
+        fontSize: 12,
+        lineHeight: 16,
+    },
+    default: {
+        fontSize: 14,
+        lineHeight: 20,
+    },
+    header1: {
+        fontSize: 80,
+    },
+    header2: {
+        fontSize: 60,
+    },
+    header3: {
+        fontSize: 40,
+    },
+    header4: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        lineHeight: 36,
+    },
+    header5: {
+        fontSize: 24,
+        lineHeight: 32,
+    },
+    header6: {
+        fontSize: 20,
+        lineHeight: 28,
+    },
+    header7: {
+        fontSize: 18,
+        lineHeight: 28,
+    },
+    label: {
+        fontSize: 11,
+        lineHeight: 16,
+    },
+});
+
+export default Text;
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,101 @@
 /**
  * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+/** Primary */
+const primary100 = '#CCE9ED';
+const primary200 = '#A2D0D6';
+const primary300 = '#76B8C0';
+const primary400 = '#71979C';
+const primary500 = '#648286';
+const primary600 = '#396368';
+const primary700 = '#144146';
+const primary800 = '#0B3D43';
+const primary900 = '#022A2F';
+
+const primary90p = '#667C7E';
+const primary80p = '#778A8D';
+const primary20p = '#DDE2E2';
+const primary10p = '#EEF0F1';
+const primary5p = '#F6F8F8';
+
+/** Secondary */
+const secondary100 = '#E1F1BC';
+const secondary200 = '#CEE993';
+const secondary300 = '#BCDE6B';
+const secondary400 = '#AFD751';
+const secondary500 = '#A3D139';
+const secondary600 = '#97BD33';
+const secondary700 = '#88A52A';
+const secondary800 = '#798D21';
+const secondary900 = '#626615';
+
+const secondary90p = '#ACD64D';
+const secondary80p = '#B5DA61';
+const secondary20p = '#EDF6D7';
+const secondary10p = '#F6FAEB';
+const secondary5p = '#FAFDF5';
+
+/** Tertiary */
+const tertiary100 = '#EEE0FB';
+const tertiary200 = '#DCC0F7';
+const tertiary300 = '#CBA1F2';
+const tertiary400 = '#B981EE';
+const tertiary500 = '#A862EA';
+const tertiary600 = '#864EBB';
+const tertiary700 = '#653B8C';
+const tertiary800 = '#43275E';
+const tertiary900 = '#22142F';
+
+const tertiary90p = '#B172EC';
+const tertiary80p = '#B981EE';
+const tertiary20p = '#EEE0FB';
+const tertiary10p = '#F6EFFD';
+const tertiary5p = '#FBF7FE';
+
+/** Dark */
+const dark500 = '#0F1414';
+const dark90P = '#272C2C';
+const dark80P = '#3F4343';
+const dark20P = '#CFD0D0';
+const dark10P = '#E7E7E7';
+const dark5P = '#F3F3F3';
+
+/** Light */
+const light500 = '#D9E1E1';
+const light90P = '#DDE4E4';
+const light80P = '#E1E7E7';
+const light20P = '#F7F9F9';
+const light10P = '#FBFCFC';
+const light5P = '#FDFDFD';
+
+/** Gray */
+const gray500 = '#9F9E9A';
+const gray90P = '#A9A8A4';
+const gray80P = '#B2B1AE';
+const gray20P = '#ECECEB';
+const gray10P = '#F5F5F5';
+const gray5P = '#FAFAFA';
+
+/** White */
+const white500 = '#FFFFFF';
+const white90P = '#FEFEFE';
+const white80P = '#FDFCFD';
+const white20P = '#F5F5F5';
+const white10P = '#F4F3F4';
+const white5P = '#F4F3F4';
+
+/** Option */
+const option1 = '#30BE82';
+const option2 = '#30BEB6';
+const option3 = '#5D30BE';
+const option4 = '#304FBE';
 
 export const Colors = {
-    light: {
-        text: '#11181C',
-        background: '#fff',
-        tint: tintColorLight,
-        icon: '#687076',
-        tabIconDefault: '#687076',
-        tabIconSelected: tintColorLight,
-    },
     dark: {
-        text: '#ECEDEE',
-        background: '#151718',
-        tint: tintColorDark,
-        icon: '#9BA1A6',
-        tabIconDefault: '#9BA1A6',
-        tabIconSelected: tintColorDark,
+        text: light500,
+    },
+    light: {
+        text: dark500,
     },
 };
